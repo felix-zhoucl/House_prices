7acdf1be34b277129f8b2ce62009d695bc012bd9
ZCL（）980304 <zhouchl01@inspur.com>
2023-03-25 14:33:09 +0800

    connect database read config file

--- environment.py
+++ environment.py
@@ -2,14 +2,54 @@
 # @Author:zcl01
 # @Create_time:2023/3/20 16:18
 # @File_name:environment
+import json
 import logging
 import os
+import traceback
 
-"""全局变量"""
+import dbutils.pooled_db as pool_db
+import pymysql
+
+"""输出日志到控制台和文件"""
 LOG_PATH = './log/'
 LOG_LEVEL = logging.DEBUG
 
-"""输出日志到控制台和文件"""
+
+def get_config(key):
+    """
+    读取配置文件信息
+    :param key:
+    :return:
+    """
+    path = "config.json"
+    try:
+        with open(path, "r", encoding="utf-8") as f:
+            data = json.load(f)
+            ret = data.get(key)
+            return ret
+    except:
+        logging.error(traceback.format_exc())
+
+
+def connect_db():
+    """
+    链接数据库
+    :return:
+    """
+    pool = pool_db.PooledDB(
+        creator=pymysql,  # 使用链接数据库的模块
+        maxconnections=6,  # 连接池允许的最大连接数，0和None表示不限制连接数
+        mincached=2,  # 初始化时，链接池中至少创建的空闲的链接，0表示不创建
+        blocking=True,  # 连接池中如果没有可用连接后，是否阻塞等待。True，等待；False，不等待然后报错
+        ping=0,
+        host=get_config("__db_host__"),
+        port=get_config("__db_port__"),
+        user=get_config("__db_user__"),
+        password=get_config("__db_pwd__"),
+        database=get_config("__db_name__"),
+        charset='utf8'
+    )
+    return pool
 
 
 def print_log(logFilename):
