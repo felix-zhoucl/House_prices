0e9daa200d99e8ab3a3eca3f2fa0f4003bebf045
zhouchl01 <zhouchl01@inspur.com>
2023-03-29 15:04:01 +0800

    add post_test

--- auth/service.py
+++ auth/service.py
@@ -6,21 +6,29 @@
 # 创建请求处理器
 # 当处理请求时会进行实例化并调用HTTP请求对应的方法
 import traceback
+from typing import Optional, Awaitable
 
-import tornado
+from tornado.web import RequestHandler
 
 
-class IndexHandler(tornado.web.RequestHandler):
+class IndexHandler(RequestHandler):
+    def data_received(self, chunk: bytes) -> Optional[Awaitable[None]]:
+        pass
+
     # 定义get方法对HTTP的GET请求做出响应
     def get(self):
-        print(self.cookies)
         try:
             # 从querystring查询字符串中获取id参数的值，若无则默认为0.
-            id = self.get_argument("id", 0)
+            _id = self.get_argument("id", 0)
             if id:
                 # write方法将字符串写入HTTP响应
-                self.write("hello world id = " + id)
+                self.write("hello world id = " + _id)
             else:
                 self.write("id is not found")
         except Exception as e:
             self.write(traceback.format_exc())
+
+    def post(self):
+        _id = self.get_body_argument("id")
+        self.write({"_id": _id})
+
